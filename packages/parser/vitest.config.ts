9774d5f2cac2763896bb0bdd271116217a803910
Paul Len <paul.len@runtimeverification.com>
2026-04-25 14:04:02 -0400

    all

--- packages/parser/vitest.config.ts
+++ packages/parser/vitest.config.ts
@@ -1,20 +1,27 @@
-import { defineConfig, mergeConfig } from "vitest/config";
-import { baseConfig } from "@repo/vitest-config/base";
+import { defineConfig } from "vitest/config";
 
 const shouldSilenceConsole = process.env.VITEST === "true";
 
-export default defineConfig(
-  mergeConfig(baseConfig, {
-    test: {
-      environment: "node",
-      include: ["tests/**/*.test.ts"],
-      onConsoleLog() {
-        if (shouldSilenceConsole) {
-          return false;
-        }
-
-        return undefined;
-      },
+export default defineConfig({
+  test: {
+    globals: true,
+    silent: "passed-only",
+    testTimeout: 15_000,
+    environment: "node",
+    include: ["tests/**/*.test.ts"],
+    exclude: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/.{idea,git,cache,output,temp}/**",
+      "**/.next/**",
+      "**/.turbo/**",
+      "**/*.pro.test.ts",
+    ],
+    onConsoleLog() {
+      if (shouldSilenceConsole) {
+        return false;
+      }
+      return undefined;
     },
-  }),
-);
+  },
+});
